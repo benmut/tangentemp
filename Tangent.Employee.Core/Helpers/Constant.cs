a22eabbdd2a541883ac9fe701b6a67c49ab1746a
Ben <benm@hollard.co.za>
2018-07-31 15:02:03 +0200

    Implemented progressdialog for authentication + saving access token in shared preferences + implemented dialog for permission denied + implemented hidesoftkeyboard method.

--- Tangent.Employee.Core/Helpers/Constant.cs
+++ Tangent.Employee.Core/Helpers/Constant.cs
@@ -5,5 +5,8 @@ namespace Tangent.Employee.Core.Helpers
     {
         public static string API_BASE_ADDRESS = "http://staging.tangent.tngnt.co/api/";
         public static string tokenEndpointUri = "http://staging.tangent.tngnt.co/api-token-auth/";
+
+        public const string Authentication = "authentication";
+        public const string AccessToken = "accesstoken";
     }
 }
--- Tangent.Employee.Droid/Activities/LoginActivity.cs
+++ Tangent.Employee.Droid/Activities/LoginActivity.cs
@@ -6,31 +6,116 @@ using System.Text;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Support.V7.App;
-using Android.Views;
 using Android.Widget;
+using SimpleInjector;
+using Tangent.Employee.Core.Helpers;
+using Tangent.Employee.Core.Services.Implementation;
+using Tangent.Employee.Core.Services.Interface;
+using Tangent.Employee.Droid.Application;
+using Tangent.Employee.Droid.Utils;
 
 namespace Tangent.Employee.Droid.Activities
 {
     [Activity(Label = "LoginActivity", MainLauncher = true, Theme = "@style/AppTheme.NoActionBar")]
     public class LoginActivity : AppCompatActivity
     {
+        static readonly string TAG = typeof(LoginActivity).Name;
+
         EditText _etUserName;
         EditText _etUserPassword;
         Button _btnLogin;
 
-        static readonly string TAG = typeof(LoginActivity).Name;
+        IUserService _userService;
+
+        private static Container Container
+        {
+            get
+            {
+                return TangentEmployeeApplication.Container;
+            }
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_login);
 
+            _userService = Container.GetInstance<UserService>();
+
             _etUserName = FindViewById<EditText>(Resource.Id.et_username);
             _etUserPassword = FindViewById<EditText>(Resource.Id.et_password);
             _btnLogin = FindViewById<Button>(Resource.Id.btn_login);
 
+            _btnLogin.Click += Login_OnClickAsync;
+            _btnLogin.Enabled = false;
+
+            _etUserName.TextChanged += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Text.ToString()) && !string.IsNullOrEmpty(_etUserPassword.Text))
+                {
+                    _btnLogin.Enabled = true;
+                }
+                else
+                {
+                    _btnLogin.Enabled = false;
+                }
+            };
+
+            _etUserPassword.TextChanged += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Text.ToString()) && !string.IsNullOrEmpty(_etUserName.Text))
+                {
+                    _btnLogin.Enabled = true;
+                }
+                else
+                {
+                    _btnLogin.Enabled = false;
+                }
+            };
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        private async void Login_OnClickAsync(object sender, EventArgs e)
+        {
+            var username = _etUserName.Text;
+            var password = _etUserPassword.Text;
+
+            // Hide Softkeyboard & disable button
+            Helpers.HideSoftKeyboard(this, this);
+            _btnLogin.Enabled = false;
+
+            var progressDialog = ProgressDialog.Show(this, null, GetString(Resource.String.txt_loggin_in));
+            progressDialog.Show();
+            var accessToken = await _userService.GetAccessTokenAsync(username, password);
+
+            if(!string.IsNullOrEmpty(accessToken))
+            {
+                progressDialog.Dismiss();
+
+                // Save Token to Shared preferences
+                var sharedPreferences = GetSharedPreferences(Constant.Authentication, FileCreationMode.Private);
+                var editor = sharedPreferences.Edit();
+                editor.PutString(Constant.AccessToken, accessToken);
+                editor.Commit();
+
+                StartActivity(typeof(MainActivity));
+            }
+            else
+            {
+                progressDialog.Dismiss();
+                DisplayPermissionDeniedError();
+            }
+            _etUserPassword.Text = "";
+        }
+
+        private void DisplayPermissionDeniedError()
+        {
+            Helpers.ShowDialog(this, GetString(Resource.String.permission_denied_title), GetString(Resource.String.error_incorrect_credentials));
         }
     }
 }
