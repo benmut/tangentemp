2e14b5a85faf39b4e61659a8e599100163fddf0c
Ben <benm@hollard.co.za>
2018-07-31 12:26:36 +0200

    implemented IoC Container to manage classes dependencies

--- Tangent.Employee.Droid/Application/TangentEmployeeApplication.cs
+++ Tangent.Employee.Droid/Application/TangentEmployeeApplication.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using Android.App;
+using Android.Runtime;
+using SimpleInjector;
+using Tangent.Employee.Core.Services.Implementation;
+using Tangent.Employee.Core.Services.Interface;
+
 namespace Tangent.Employee.Droid.Application
 {
-    public class TangentEmployeeApplication
+    [Application]
+    public class TangentEmployeeApplication : Android.App.Application
     {
-        public TangentEmployeeApplication()
+        public static Container Container { get; set; }
+
+        public TangentEmployeeApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
         {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            // 1. Create a new Simple Injector Container
+            Container = new Container();
+
+            // 2. Configure the container
+            Container.Register<IUserService, UserService>(Lifestyle.Singleton);
+
+            // 3. Verify your configuration
+            Container.Verify();
         }
     }
 }
