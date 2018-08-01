aaa7df17c750e949a318413bec7735ba66f67e9f
Ben <benm@hollard.co.za>
2018-08-01 10:55:42 +0200

    implemented employee service

--- Tangent.Employee.Core/Services/Implementation/EmployeeService.cs
+++ Tangent.Employee.Core/Services/Implementation/EmployeeService.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tangent.Employee.Core.Helpers;
+using Tangent.Employee.Core.Services.Interface;
+
 namespace Tangent.Employee.Core.Services.Implementation
 {
-    public class EmployeeService
+    public class EmployeeService : IEmployeeService
     {
-        public EmployeeService()
+        private static HttpClient client = new HttpClient()
         {
+            BaseAddress = new Uri(Constant.API_BASE_ADDRESS)
+        };
+
+        static EmployeeService()
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<IList<Models.Employee>> GetAllEmployeesAsync(string accessToken)
+        {
+            IList<Models.Employee> result = null;
+
+            string queryString = "employee/";
+            try
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", accessToken);
+
+                var response = await client.GetStringAsync(queryString);
+                if (string.IsNullOrEmpty(response))
+                {
+                    return null;
+                }
+
+                result = JsonConvert.DeserializeObject<IList<Models.Employee>>(response);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Message: {e.Message} \n Query String: {queryString}");
+            }
+
+            return result;
         }
     }
 }
--- Tangent.Employee.Core/Services/Interface/IEmployeeService.cs
+++ Tangent.Employee.Core/Services/Interface/IEmployeeService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Tangent.Employee.Core.Services.Interface
 {
     public interface IEmployeeService
     {
+        Task<IList<Models.Employee>> GetAllEmployeesAsync(string accessToken);
     }
 }
