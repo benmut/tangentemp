8f942dd95d0dc0949be32b083615b3bb7288fe5a
Ben <benm@hollard.co.za>
2018-08-04 11:36:24 +0200

    added methods to employee service

--- Tangent.Employee.Core/Services/Implementation/EmployeeService.cs
+++ Tangent.Employee.Core/Services/Implementation/EmployeeService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Tangent.Employee.Core.Helpers;
+using Tangent.Employee.Core.Models;
 using Tangent.Employee.Core.Services.Interface;
 
 namespace Tangent.Employee.Core.Services.Implementation
@@ -45,6 +47,27 @@ namespace Tangent.Employee.Core.Services.Implementation
             }
 
             return result;
+        }
+
+        public async Task<IEnumerable<Models.Employee>> GetBirthdays(IList<Models.Employee> employees)
+        {
+            string CurrentMonth = DateTime.Now.Month.ToString();
+            if(CurrentMonth.Length == 1)
+            {
+                CurrentMonth = "0" + CurrentMonth;
+            }
+
+            return await Task.FromResult(employees.Where(e => e.Birthdate.Substring(5,2) == CurrentMonth));
+        }
+
+        public async Task<IEnumerable<Models.Employee>> GetEmployeeByGender(IList<Models.Employee> employees, string gender)
+        {
+            return await Task.FromResult(employees.Where(e => e.Gender == gender));
+        }
+
+        public async Task<IEnumerable<Models.Employee>> GetEmployeePosition(IList<Models.Employee> employees, string position)
+        {
+            return await Task.FromResult(employees.Where(e => e.Position.Name  == position));
         }
     }
 }
--- Tangent.Employee.Core/Services/Interface/IEmployeeService.cs
+++ Tangent.Employee.Core/Services/Interface/IEmployeeService.cs
@@ -7,5 +7,8 @@ namespace Tangent.Employee.Core.Services.Interface
     public interface IEmployeeService
     {
         Task<IList<Models.Employee>> GetAllEmployeesAsync(string accessToken);
+        Task<IEnumerable<Models.Employee>> GetBirthdays(IList<Models.Employee> employees);
+        Task<IEnumerable<Models.Employee>> GetEmployeeByGender(IList<Models.Employee> employees, string gender);
+        Task<IEnumerable<Models.Employee>> GetEmployeePosition(IList<Models.Employee> employees, string position);
     }
 }
