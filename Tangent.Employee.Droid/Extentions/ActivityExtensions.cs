5472893107ac56d2f0a1d01b4cbe6ba0bee448b9
Ben <benm@hollard.co.za>
2018-08-04 11:27:05 +0200

    avtivity extension for toolbar title

--- Tangent.Employee.Droid/Extentions/ActivityExtensions.cs
+++ Tangent.Employee.Droid/Extentions/ActivityExtensions.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using Android.App;
+using Android.Widget;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
 namespace Tangent.Employee.Droid.Extentions
 {
-    public class ActivityExtensions
+    public static class ActivityExtensions 
     {
-        public ActivityExtensions()
+        public static void SetCustomTitle(this Activity activity, string title)
         {
+            var toolbar = activity.FindViewById<Toolbar>(Resource.Id.toolbar);
+            var textTitle = toolbar?.FindViewById<TextView>(Resource.Id.toolbar_title);
+            if (textTitle != null)
+            {
+                textTitle.Text = title;
+            }
         }
     }
 }
