cc0f7cb73034dc045dbe4707fb828c0b61e00f78
Ben <benm@hollard.co.za>
2018-08-04 11:34:05 +0200

    Droid + Core csproj files

--- Tangent.Employee.Droid/Activities/MainActivity.cs
+++ Tangent.Employee.Droid/Activities/MainActivity.cs
@@ -7,6 +7,12 @@ using Android.Support.V7.App;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
 using Fragment = Android.Support.V4.App.Fragment;
 using Tangent.Employee.Droid.Fragments;
+using SimpleInjector;
+using Tangent.Employee.Droid.Application;
+using Tangent.Employee.Core.Services.Interface;
+using System;
+using Tangent.Employee.Core.Services.Implementation;
+using System.Collections.Generic;
 
 namespace Tangent.Employee.Droid
 {
@@ -19,10 +25,22 @@ namespace Tangent.Employee.Droid
         private ActionBarDrawerToggle _drawerToggle;
         private Toolbar _toolBar;
 
+        IEmployeeService _employeeService;
+
+        private static Container Container
+        {
+            get
+            {
+                return TangentEmployeeApplication.Container;
+            }
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            IocContainterGetInstances();
 
             _drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
             var _navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
@@ -30,7 +48,12 @@ namespace Tangent.Employee.Droid
 
             SetupNavigationDrawer();
 
-            ReplaceFragment(new DashboardFragment());
+            ReplaceFragment(DashboardFragment.NewInstance(_employeeService, OnTileSelected));
+        }
+
+        private void IocContainterGetInstances()
+        {
+            _employeeService = Container.GetInstance<EmployeeService>();
         }
 
         private void SetupNavigationDrawer()
@@ -44,6 +67,12 @@ namespace Tangent.Employee.Droid
             _drawerToggle.DrawerIndicatorEnabled = true;
             _drawer.AddDrawerListener(_drawerToggle);
             _drawerToggle.SyncState(); 
+        }
+
+        private void OnTileSelected(List<Core.Models.Employee> employees)
+        {
+            Fragment fragment = EmployeesCategoryFragment.NewInstance(employees);
+            ReplaceFragment(fragment);
         }
 
         private void ReplaceFragment(Fragment fragment)
