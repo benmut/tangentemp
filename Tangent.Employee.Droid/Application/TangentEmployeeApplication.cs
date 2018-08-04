a8915b53023432b7c93d60ffa3978d91fc99e8d7
Ben <benm@hollard.co.za>
2018-08-04 11:28:00 +0200

    added access token in application setting

--- Tangent.Employee.Droid/Application/TangentEmployeeApplication.cs
+++ Tangent.Employee.Droid/Application/TangentEmployeeApplication.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Android.App;
+using Android.Content;
 using Android.Runtime;
 using SimpleInjector;
+using Tangent.Employee.Core.Helpers;
 using Tangent.Employee.Core.Services.Implementation;
 using Tangent.Employee.Core.Services.Interface;
 
@@ -11,6 +13,16 @@ namespace Tangent.Employee.Droid.Application
     public class TangentEmployeeApplication : Android.App.Application
     {
         public static Container Container { get; set; }
+
+        public string AccessToken
+        {
+            get
+            {
+                // Retrieve Token from Shared preferences
+                var sharedPreferences = GetSharedPreferences(Constant.Authentication, FileCreationMode.Private);
+                return sharedPreferences.GetString(Constant.AccessToken, string.Empty);
+            }
+        }
 
         public TangentEmployeeApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
         {
@@ -25,6 +37,7 @@ namespace Tangent.Employee.Droid.Application
 
             // 2. Configure the container
             Container.Register<IUserService, UserService>(Lifestyle.Singleton);
+            Container.Register<IEmployeeService, EmployeeService>(Lifestyle.Singleton);
 
             // 3. Verify your configuration
             Container.Verify();
