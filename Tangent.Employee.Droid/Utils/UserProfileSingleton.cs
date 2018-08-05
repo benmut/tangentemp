b8635b40e2635fc9fa9bf964d40cfe27c2466548
Ben <benm@hollard.co.za>
2018-08-05 12:29:43 +0200

    user profile singleton

--- Tangent.Employee.Droid/Utils/UserProfileSingleton.cs
+++ Tangent.Employee.Droid/Utils/UserProfileSingleton.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using Tangent.Employee.Core.Models;
+
 namespace Tangent.Employee.Droid.Utils
 {
     public class UserProfileSingleton
     {
+        private UserProfile userProfile = null;
+
+        private static UserProfileSingleton _instance = new UserProfileSingleton();
+
         public UserProfileSingleton()
         {
+        }
+
+        public static UserProfileSingleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public UserProfile GetUserProfile()
+        {
+            return userProfile;
+        }
+
+        public void SetUserProfile(UserProfile userProfile)
+        {
+            this.userProfile = userProfile;
         }
     }
 }
