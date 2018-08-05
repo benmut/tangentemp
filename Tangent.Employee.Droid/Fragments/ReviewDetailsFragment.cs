a07b87be5d07ae30a5c0b6882709947c73a483d3
Ben <benm@hollard.co.za>
2018-08-05 12:25:57 +0200

    review details fragment

--- Tangent.Employee.Droid/Fragments/ReviewDetailsFragment.cs
+++ Tangent.Employee.Droid/Fragments/ReviewDetailsFragment.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,24 +10,61 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Tangent.Employee.Core.Models;
+using Tangent.Employee.Droid.Extentions;
+using Fragment = Android.Support.V4.App.Fragment;
 
 namespace Tangent.Employee.Droid.Fragments
 {
     public class ReviewDetailsFragment : Fragment
     {
+        readonly string TAG = typeof(ReviewDetailsFragment).FullName;
+
+        TextView _tvId;
+        TextView _tvDate;
+        TextView _tvSalary;
+        TextView _tvType;
+
+        Review _review;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+        }
 
-            // Create your fragment here
+        public static Fragment NewInstance(Review review)
+        {
+            var fragment = new ReviewDetailsFragment();
+            fragment._review = review;
+            return fragment;
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            View view = inflater.Inflate(Resource.Layout.fragment_review_details, container, false);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            _tvId = (TextView)view.FindViewById(Resource.Id.tv_id);
+            _tvDate = (TextView)view.FindViewById(Resource.Id.tv_date);
+            _tvSalary = (TextView)view.FindViewById(Resource.Id.tv_salary);
+            _tvType = (TextView)view.FindViewById(Resource.Id.tv_type);
+
+            return view;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+            (this.Activity as MainActivity).SetCustomTitle("Review Details");
+
+            _tvId.Text = _review.Id;
+            _tvDate.Text = _review.Date;
+            _tvSalary.Text = _review.Salary;
+            _tvType.Text = _review.Type;
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
         }
     }
 }
