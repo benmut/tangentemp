20b80965888912b970733330754cb7a5b5355328
Ben <benm@hollard.co.za>
2018-08-05 12:19:39 +0200

    profile service interface + implementation

--- Tangent.Employee.Core/Services/Implementation/ProfileService.cs
+++ Tangent.Employee.Core/Services/Implementation/ProfileService.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tangent.Employee.Core.Helpers;
+using Tangent.Employee.Core.Models;
+using Tangent.Employee.Core.Services.Interface;
+
 namespace Tangent.Employee.Core.Services.Implementation
 {
-    public class ProfileService
+    public class ProfileService : IProfileService
     {
-        public ProfileService()
+        private static HttpClient client = new HttpClient()
         {
+            BaseAddress = new Uri(Constant.API_BASE_ADDRESS)
+        };
+
+        static ProfileService()
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<UserProfile> GetUserProfile(string accessToken)
+        {
+            UserProfile result = null;
+
+            string queryString = "employee/me/";
+            try
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", accessToken);
+
+                var response = await client.GetStringAsync(queryString);
+                if (string.IsNullOrEmpty(response))
+                {
+                    return null;
+                }
+
+                result = JsonConvert.DeserializeObject<UserProfile>(response);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Message: {e.Message} \n Query String: {queryString}");
+            }
+
+            return result;
         }
     }
 }
--- Tangent.Employee.Core/Services/Interface/IProfileService.cs
+++ Tangent.Employee.Core/Services/Interface/IProfileService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Tangent.Employee.Core.Models;
 
 namespace Tangent.Employee.Core.Services.Interface
 {
     public interface IProfileService
     {
+        Task<UserProfile> GetUserProfile(string accessToken);
     }
 }
