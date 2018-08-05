fec8cb294e7286ec4348ab4651acf4535d6f5634
Ben <benm@hollard.co.za>
2018-08-05 12:17:40 +0200

    review model

--- Tangent.Employee.Core/Models/Review.cs
+++ Tangent.Employee.Core/Models/Review.cs
@@ -3,8 +3,9 @@ namespace Tangent.Employee.Core.Models
 {
     public class Review
     {
-        public Review()
-        {
-        }
+        public string Id { get; set; }
+        public string Date { get; set; }
+        public string Salary { get; set; }
+        public string Type { get; set; }
     }
 }
